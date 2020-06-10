c8b796445dcde5d16c83e607832043150f6131a7
Ernesto <ertsaz9009@gmail.com>
2020-06-10 17:01:57 -0500

    se crea la condicion para mostrar las X y O n el jego

--- Game.js
+++ Game.js
@@ -1,14 +1,14 @@
-let Tablero;
+let tablero;
 
 // DESCRITCION: Crea el espacio de trabajo desde p5.js 
 function setup() {
     createCanvas(400, 400);
     w = width / 3;
     h = height / 3;
-    Tablero = new Tablero();
+    tablero = new Tablero();
 }
 
-// DESCRIPCION : funcion que dibuja el tablero del juego
-function pantalla(){
-    Tablero.Mostrar();
+// DESCRIPCION : funcion que dibuja el tablero del juego en la pagina web
+function draw(){
+    tablero.Mostrar();
 }
--- Tablero.js
+++ Tablero.js
@@ -1,7 +1,8 @@
 class Tablero {
     constructor() {
+
         this.tablero = [
-            ['', '', ''],
+            ['O', 'X', ''],
             ['', '', ''],
             ['', '', '']
         ];
@@ -13,10 +14,31 @@ class Tablero {
         background(255);
         strokeWeight(4);
 
+        // Creacion de la cuadricula del juego #
         line(this.w, 0, this.w, height);
         line(this.w * 2, 0, this.w * 2, height);
         line(0, this.h, width, this.h);
         line(0, this.h * 2, width, this.h * 2);
-    }
 
+        // Se muestran las X y O de los respectivos jugadores
+        for (let i = 0; i < 3; ++i) {
+            for (let j = 0; j < 3; ++j) {
+
+                let x = (this.w * j) + (this.w / 2);
+                let y = (this.h * i) + (this.h / 2);
+                let xr = this.w / 4;
+                let yr = this.h / 4;
+
+                if (this.tablero[i][j] == 'X') {
+                    
+                    line(x - xr, y - yr, x + xr, y + yr);
+                    line(x + xr, y - yr, x - xr, y + yr);
+                }
+                else if (this.tablero[i][j] == 'O') {
+                    ellipseMode(CENTER);
+                    ellipse(x, y, xr * 2, yr * 2);
+                }
+            }
+        }
+    }   
 }
